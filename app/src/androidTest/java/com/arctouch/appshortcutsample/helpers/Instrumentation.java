570d933b07ee4e5efc67bcdcad22250f75f055ff
Thiago Cechetto <thiago.cechetto@arctouch.com>
2017-01-31 16:20:13 -0200

    updated Instrumentation to work with both Cucumber tests and Junit tests

--- app/src/androidTest/java/com/arctouch/appshortcutsample/helpers/Instrumentation.java
+++ app/src/androidTest/java/com/arctouch/appshortcutsample/helpers/Instrumentation.java
@@ -9,18 +9,34 @@ import android.support.test.runner.AndroidJUnitRunner;
 
 public class Instrumentation extends AndroidJUnitRunner {
 
-    private final CucumberInstrumentationCore instrumentationCore = new CucumberInstrumentationCore(this);
+  private final CucumberInstrumentationCore instrumentationCore;
+  private boolean finishedCucumberInstrumentation = false;
 
-    @Override
-    public void onCreate(final Bundle bundle) {
-        instrumentationCore.create(bundle);
-        super.onCreate(bundle);
+  public Instrumentation() {
+    instrumentationCore = new CucumberInstrumentationCore(this);
+  }
+
+  @Override
+  public void onCreate(final Bundle bundle) {
+    instrumentationCore.create(bundle);
+    super.onCreate(bundle);
+  }
+
+  @Override
+  public void onStart() {
+    finishedCucumberInstrumentation = false;
+    waitForIdleSync();
+    instrumentationCore.start();
+  }
+
+  @Override
+  public void finish(int resultCode, Bundle results) {
+    if (!finishedCucumberInstrumentation) {
+      finishedCucumberInstrumentation = true;
+      super.onStart();
+    } else {
+      super.finish(resultCode, results);
     }
 
-    @Override
-    public void onStart() {
-        waitForIdleSync();
-//        super.onStart();
-        instrumentationCore.start();
-    }
+  }
 }
